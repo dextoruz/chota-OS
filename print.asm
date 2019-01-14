314e0c9bce2e9ab594af08823d1e9e4c37945439
linxnerd <linxnerd@outlook.com>
2019-01-14 11:57:21 +0500

    minor changes in 'print.asm'

--- print.asm
+++ print.asm
@@ -1,29 +1,33 @@
+;;;;;;; input string ';;;;;;;;;;;;'
 get_string:
-mov ah,0eh
+  mov ah,0eh
 
   .loop3:
     lodsb
     cmp al,0
-    je next
+    je loop
     int 10h
-  jmp .loop3
+   jmp .loop3
 
-  next:
-    mov cl, 0
-    .loop:
+    loop:
+
       mov ah,00h
       int 16h
       mov [si+27], al
+      cmp al, 0dh
+      je done
       mov ah,0eh
       int 10h
       inc si
-      cmp cl,15
-      je .done
-      inc cl
-      jmp .loop
-  .done:
+      jmp loop
+
+  done:
     mov cl,0
+
 ret
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+;;;;;;;;;;;Output string;;;;;;;;;;;;;;;
 
 print_string:
   mov ah,0eh
@@ -38,12 +42,18 @@ print_string:
   halt:
     cli
     hlt
-ret
 
+ret
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+;;;;; endl ;;;;;;;;;;;;;;;;;;;;;;;;;
 next_line:
+
   mov ah,0eh
   mov al,0dh
   int 10h
   mov al,0ah
   int 10h
+
 ret
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
